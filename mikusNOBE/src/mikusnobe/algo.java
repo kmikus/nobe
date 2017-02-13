bb1e83299c6e297c3fcf406d85a818bd08529ffd
Kevin Mikus <kevinpmikus@gmail.com>
2017-02-13 13:29:12 -0500

    added comments, TODO to exisiting methods

--- mikusNOBE/src/mikusnobe/algo.java
+++ mikusNOBE/src/mikusnobe/algo.java
@@ -14,7 +14,7 @@ import java.math.BigInteger;
 
 public class algo {
     
-	//retrieves byte array from input file
+    //retrieves byte array from input file
     public static byte[] getBytes(String path) throws IOException {
         Path filePath = Paths.get(path);
         byte[] fileBytes = Files.readAllBytes(filePath);
@@ -35,7 +35,9 @@ public class algo {
         return outString.reverse().toString();
     }
     
-    //creates large binary string from byte array
+    //creates large binary string from byte array, param to specify number of bytes
+    //TODO overload method to take entire file to convert to one string
+    //TODO refactor method to give String[] of binary based on num of bytes
     public static String bytesToBigBinString(byte[] inBytes, int numOfBytes) {
         String bigBinString = "";
         for (int i=0; i<numOfBytes; i++) {
@@ -47,6 +49,7 @@ public class algo {
     }
 	
     //reverse of previous
+    //TODO overload to accept input of String[]
     public static byte[] bigBinStringToBytes(String bigBinString) {
         String tempStr = "";
         byte[] outputBytes = new byte[bigBinString.length()/8];
@@ -57,9 +60,10 @@ public class algo {
     }
     
     //takes any binary string and converts to int
-    public static int binToInt(String bigBinString) {
+    //TODO is this necessary?
+    public static int binToLong(String bigBinString) {
         char[] binAsChar = bigBinString.toCharArray();
-        int sum = 0;
+        long sum = 0;
         for (int i=0; i<binAsChar.length; i++) {
             if(binAsChar[i]=='1') {
                 sum+=Math.pow(2, i);
@@ -68,6 +72,7 @@ public class algo {
         return sum;
     }
     
+    //overloaded method to indicate direction, use 'r' to start from right
     public static long binToLong(String bigBinString, char direction) {
         if (direction == 'r') {
                 bigBinString = revString(bigBinString);
@@ -82,11 +87,18 @@ public class algo {
         return sum;
     }
     
+	//creates a big integer from an input string
+	//TODO bigint operations
     public static BigInteger strToBigInt(String bigString) {
         BigInteger bigInt = new BigInteger(bigString);
         return bigInt;
     }
     
+	//TODO use max size to get close to bigint, then subtract and iterate through for loop
+	//possibly use array and index to imitate subtraction, seperate method?
+	//outer outer while loop - set threshold to arbitrary value (1 byte? = 255 int) if less, byte[] outfile
+	//outer for loop - iterate through bases, start with getMax(currentVal), reset counter to 10^i
+	//inner loop - try each base to get as close as possible
     public static int[] nobe(BigInteger bigInt, int streamSize) {
         ArrayList<Integer> temp = new ArrayList();
         long max = getMax(streamSize);
@@ -94,6 +106,8 @@ public class algo {
         return null;
     }
     
+	//converts bin string to as close as possible (pad to 19 digits long) and concatenate to shorten overall chars used
+	//might take too long
     public static String binToFakeLong(String bigBinString) {
         char[] binAsChar = bigBinString.toCharArray();
         long sum = 0;
@@ -119,6 +133,7 @@ public class algo {
         return fakeLong;
     }
     
+	//TODO method to convert from fake long bake to binary string
 //    public static String fakeLongtoBin(String fakeLong) {
 //        String binString = "";
 //        for (int i=0; i<fakeLong.length()/19; i++) {
@@ -126,6 +141,8 @@ public class algo {
 //        }
 //    }
     
+	//max possible int value if all bytes are 1
+	//TODO accept bigint as param
     public static long getMax(int numOfBytes) {
         String ones = "";
         for(int i=0; i<numOfBytes*8; i++) {
@@ -134,8 +151,4 @@ public class algo {
         return binToInt(ones);
     }
     
-    public static byte[] nobeComp(int bigNum) {
-        ArrayList<Integer> keyList = new ArrayList<Integer>();
-        return null;
-    }
 }
